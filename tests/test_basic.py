e4e5e61b06c68ebf10a9d99a64b6cad53a02ca62
Calvin Grabowski <calvingrabowski@gmail.com>
2026-02-22 14:32:32 -0800

    made a test file to see how we could run the tests with github workflow

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,8 @@
 # Placeholder to ensure this folder exists in Git for Structure
 
 # make sure to replace this file when we add tests
+
+# for testing purposes:
+
+def test_sanity():
+    assert True
