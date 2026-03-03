fc56d7608967f21e460a5c1f96e5de5a1804f5da
LegendaryGithub <techbub619@gmail.com>
2026-03-03 11:01:32 -0800

    Converted .js to .jsx to work with astro. Made small changes to website, working implementing fixes for it.

--- edgeguard-frontend/src/components/Event_Feed.jsx
+++ edgeguard-frontend/src/components/Event_Feed.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import EventCard from './EventCard';
+import EventCard from './Event_Card.jsx';
 
 const EventFeed = () => {
   const [events, setEvents] = useState([]);
